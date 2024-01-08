0d2259cf05d118adbb474863c6536f025d1c25f4
egoist <rikhsiboyevzaxa@gmail.com>
2024-01-08 16:54:33 +0500

    jenkins

---
+++ src/main/java/org/example/jenkins/CalculatorController.java
@@ -0,0 +1,13 @@
+package org.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/example/jenkins/CalculatorControllerTest.java
@@ -0,0 +1,37 @@
+package org.example.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorControllerTest {
+
+    @Test
+    void add() throws IOException, InterruptedException {
+        int a = 10;
+        int b = 20;
+
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest request = HttpRequest
+
+                .newBuilder(URI.create("http://localhost:8081/add?a=%d&b=%d".formatted(a, b)))
+                .GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String body = response.body();
+        int result = Integer.parseInt(body);
+
+        Assertions.assertEquals(a + b, result);
+
+    }
+
+}
